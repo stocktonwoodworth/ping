ab5a0400507b98cd440f70f51fefd2a354145603
Stockton <stocktonwoodwort@u.boisestate.edu>
2022-11-29 22:27:51 -0700

    merged run.sh and clean.sh

--- run.sh
+++ run.sh
@@ -8,4 +8,24 @@ scala ping 							          # Run empty args
 scala ping -h 						        # Run -h arg
 scala ping --help 					      # Run --help arg
 
+echo "Cleaning environment..."
+
+# Hard code file names
+file1="/ping$.class"
+file2="/ping.class"
+
+# Get current path
+path1=$PWD$file1
+path2=$PWD$file2
+
+# Testing path
+#echo $path1
+#echo $path2
+
+# Clean up
+rm $path1
+rm $path2
+
+echo "Cleaning complete"
+
 echo "Script finished"
